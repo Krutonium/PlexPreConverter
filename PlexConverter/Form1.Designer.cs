d8f53b6b17235d0cf65478277aac8df098d0e58e
Krutonium <PFCKrutonium@gmail.com>
2019-03-19 18:32:30 -0400

    Add project files.

---
+++ PlexConverter/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PlexConverter
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDirectory = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.chkTwoPass = new System.Windows.Forms.CheckBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.cbRemoveOriginal = new System.Windows.Forms.CheckBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.pbFile = new System.Windows.Forms.ProgressBar();
+            this.ProgressBarUpdater = new System.Windows.Forms.Timer(this.components);
+            this.cbOverwrite = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbAllFiles = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDirectory
+            // 
+            this.btnDirectory.Location = new System.Drawing.Point(12, 12);
+            this.btnDirectory.Name = "btnDirectory";
+            this.btnDirectory.Size = new System.Drawing.Size(94, 23);
+            this.btnDirectory.TabIndex = 0;
+            this.btnDirectory.Text = "Browse Input";
+            this.btnDirectory.UseVisualStyleBackColor = true;
+            this.btnDirectory.Click += new System.EventHandler(this.BtnDirectory_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(112, 14);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(675, 20);
+            this.tbPath.TabIndex = 1;
+            // 
+            // chkTwoPass
+            // 
+            this.chkTwoPass.AutoSize = true;
+            this.chkTwoPass.Location = new System.Drawing.Point(12, 65);
+            this.chkTwoPass.Name = "chkTwoPass";
+            this.chkTwoPass.Size = new System.Drawing.Size(73, 17);
+            this.chkTwoPass.TabIndex = 2;
+            this.chkTwoPass.Text = "Two Pass";
+            this.chkTwoPass.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(713, 167);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // cbRemoveOriginal
+            // 
+            this.cbRemoveOriginal.AutoSize = true;
+            this.cbRemoveOriginal.Location = new System.Drawing.Point(91, 65);
+            this.cbRemoveOriginal.Name = "cbRemoveOriginal";
+            this.cbRemoveOriginal.Size = new System.Drawing.Size(104, 17);
+            this.cbRemoveOriginal.TabIndex = 4;
+            this.cbRemoveOriginal.Text = "Remove Original";
+            this.cbRemoveOriginal.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(12, 36);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(94, 23);
+            this.btnOutput.TabIndex = 5;
+            this.btnOutput.Text = "Browse Output";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.BtnOutput_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(112, 38);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(675, 20);
+            this.tbOutput.TabIndex = 6;
+            // 
+            // pbFile
+            // 
+            this.pbFile.Location = new System.Drawing.Point(12, 127);
+            this.pbFile.Name = "pbFile";
+            this.pbFile.Size = new System.Drawing.Size(775, 23);
+            this.pbFile.TabIndex = 7;
+            // 
+            // ProgressBarUpdater
+            // 
+            this.ProgressBarUpdater.Tick += new System.EventHandler(this.ProgressBarUpdater_Tick);
+            // 
+            // cbOverwrite
+            // 
+            this.cbOverwrite.AutoSize = true;
+            this.cbOverwrite.Location = new System.Drawing.Point(201, 65);
+            this.cbOverwrite.Name = "cbOverwrite";
+            this.cbOverwrite.Size = new System.Drawing.Size(71, 17);
+            this.cbOverwrite.TabIndex = 8;
+            this.cbOverwrite.Text = "Overwrite";
+            this.cbOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(738, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "This File:";
+            // 
+            // pbAllFiles
+            // 
+            this.pbAllFiles.Location = new System.Drawing.Point(12, 85);
+            this.pbAllFiles.Name = "pbAllFiles";
+            this.pbAllFiles.Size = new System.Drawing.Size(775, 23);
+            this.pbAllFiles.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(709, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Total Progress:";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(632, 167);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 12;
+            this.btnStop.Text = "Cancel";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(12, 153);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(93, 13);
+            this.lblFileName.TabIndex = 13;
+            this.lblFileName.Text = "File Name Here :P";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 202);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbAllFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbOverwrite);
+            this.Controls.Add(this.pbFile);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.cbRemoveOriginal);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.chkTwoPass);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnDirectory);
+            this.Name = "frmMainForm";
+            this.Text = "Plex Pre Converter";
+            this.Load += new System.EventHandler(this.FrmMainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDirectory;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.CheckBox chkTwoPass;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.CheckBox cbRemoveOriginal;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.ProgressBar pbFile;
+        private System.Windows.Forms.Timer ProgressBarUpdater;
+        private System.Windows.Forms.CheckBox cbOverwrite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbAllFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblFileName;
+    }
+}
+
---
+++ PlexConverter/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HandbrakeCLIwrapper;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+namespace PlexConverter
+{
+    public partial class frmMainForm : Form
+    {
+        static HandbrakeCli conv = new HandbrakeCli();
+        static HandbrakeCliConfigBuilder HBConfig = new HandbrakeCliConfigBuilder();
+        static bool wait = false;
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+        private void FrmMainForm_Load(object sender, EventArgs e)
+        {
+
+            if (!File.Exists("./HandbrakeCLI.exe"))
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://download2.handbrake.fr/1.2.2/HandBrakeCLI-1.2.2-win-x86_64.zip", "./CLI.zip");
+                    ZipFile.ExtractToDirectory("./CLI.zip", "./");
+                    File.Delete("./CLI.zip");
+                    System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo("./doc");
+                    Empty(directory);
+                    Directory.Delete("./doc");
+                }
+            }
+
+            conv.TranscodingStarted += Conv_TranscodingStarted;
+            conv.TranscodingCompleted += Conv_TranscodingCompleted;
+            conv.TranscodingError += Conv_TranscodingError;
+
+            HandbrakeCliConfigBuilder config = new HandbrakeCliConfigBuilder();
+            config.Encoder = HandbrakeCLIwrapper.Encoder.x264;
+            config.AudioCopyMask.Add(AudioCopyMask.aac);
+            config.AudioEncoder = AudioEncoder.av_aac;
+            config.Format = Format.av_mp4;
+            config.WebOptimize = true;
+            HBConfig = config;
+        }
+
+        public static void Empty(System.IO.DirectoryInfo directory)
+        {
+            foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
+            foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+        }
+
+        private void Conv_TranscodingError(object sender, HandbrakeTranscodingEventArgs e)
+        {
+
+        }
+
+        private void Conv_TranscodingCompleted(object sender, HandbrakeTranscodingEventArgs e)
+        {
+            wait = false;
+        }
+
+        private void Conv_TranscodingStarted(object sender, HandbrakeTranscodingEventArgs e)
+        {
+
+        }
+
+        private void BtnDirectory_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                tbPath.Text = fbd.SelectedPath;
+            };
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            HBConfig.TwoPass = chkTwoPass.Checked;
+            HBConfig.Build();
+            DirectoryInfo info = new DirectoryInfo(tbPath.Text);
+            List<string> files = GetFilesByExtensions(info, ".mp4",".mkv",".m4a",".avi",".3gp",".flv",".m4v",".mov",".wmv");
+            ProgressBarUpdater.Enabled = true;
+            pbAllFiles.Value = 0;
+            pbAllFiles.Maximum = files.Count();
+            foreach (var file in files)
+            {
+                conv.Transcode(HBConfig, file, tbOutput.Text, null, cbOverwrite.Checked, cbRemoveOriginal.Checked);
+                lblFileName.Text = Path.GetFileName(file);
+                wait = true;
+                while (wait)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    Application.DoEvents();
+                }
+                pbAllFiles.Value += 1;
+            }
+        }
+        public static List<string> GetFilesByExtensions(DirectoryInfo dir, params string[] extensions)
+        {
+            List<string> files = Directory.GetFiles(dir.FullName, "*.*", SearchOption.AllDirectories).Where(s => extensions.Contains(Path.GetExtension(s))).ToList();
+            return files;
+        }
+        
+       
+        private void BtnOutput_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbOutput.Text = fbd.SelectedPath;
+            };
+        }
+
+        private void ProgressBarUpdater_Tick(object sender, EventArgs e)
+        {
+            pbFile.Value = (int)conv.Status.Percentage;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            conv.StopTranscoding();
+        }
+    }
+}
